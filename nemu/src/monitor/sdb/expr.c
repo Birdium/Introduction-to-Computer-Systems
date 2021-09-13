845da5d6fb9aea33a032b80c99d884b199a4c929
tracer-ics2021 <tracer@njuics.org>
2021-09-13 12:28:19 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: d65937a72f0b4daaa0ae54029ed8ed12   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  12:28:19 up  1:00,  1 user,  load average: 0.35, 0.28, 0.30

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -90,7 +90,7 @@ static bool make_token(char *e) {
           case TK_NOTYPE : break;
           case TK_DEC : 
             tokens[nr_token].type = rules[i].token_type;
-            if (substr_len < 32) strncpy(tokens[nr_token].str, substr_start, substr_len + 1);
+            if (substr_len < 32) strncpy(tokens[nr_token].str, substr_start, substr_len);
             else {
               printf("too large int : %s\n", substr_start);
             }
