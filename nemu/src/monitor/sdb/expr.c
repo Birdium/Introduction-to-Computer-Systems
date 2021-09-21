385a1ae8a641b4b286923dc101e6873db7468fd8
tracer-ics2021 <tracer@njuics.org>
2021-09-21 20:18:04 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 7d2d24e02ee5429b977c75befb684e34   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  20:18:04 up  7:24,  1 user,  load average: 1.12, 0.81, 0.68

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -250,7 +250,7 @@ void check_expr(){
   FILE *fp = fopen("./tools/gen-expr/input", "r");
   uint32_t ans; char buf[65536+128];
   while(1){
-    if (fscanf(fp, "%u", &ans) == EOF || fgets(buf, 65536+128, fp) == NULL) break;
+    if (fscanf(fp, "%u %[^\n]s", &ans, buf) == EOF) break;
     printf("%s\n", buf);
     bool suc = true;
     uint32_t myexpr = expr(buf, &suc);
