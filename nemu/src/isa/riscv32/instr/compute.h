e0e9c42c4dffbf340cc35c5da836b42772417dc1
tracer-ics2021 <tracer@njuics.org>
2021-10-02 23:10:17 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 2303b4cf3620466984aea84ebd02a75b   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  23:10:17 up 40 min,  1 user,  load average: 0.80, 0.69, 0.52

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -1,5 +1,10 @@
-def_EHelper(lui) {
+def_EHelper(lui){
   rtl_li(s, ddest, id_src1->imm);
+}
+
+def_EHelper(auipc){
+  rtl_li(s, s0, id_src1->imm);
+  rtl_add(s, ddest, &cpu.pc, s0);
 }
 
 def_EHelper(addi){
