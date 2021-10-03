6ecff623b75efd9117ab0d0065ba8b4c44f6c11a
tracer-ics2021 <tracer@njuics.org>
2021-10-04 01:24:20 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: b2e138986f484b37b445a1d9cb629c05   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  01:24:20 up  4:11,  1 user,  load average: 0.48, 0.43, 0.47

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -1,7 +1,7 @@
 #include <cpu/decode.h>
 #include "../local-include/rtl.h"
 
-#define INSTR_LIST(f) f(lui) f(auipc) f(jal) f(jalr) f(beq) f(lw) f(sw) \
+#define INSTR_LIST(f) f(lui) f(auipc) f(jal) f(jalr) f(beq) f(bne) f(lw) f(sw) \
 f(addi) f(slti) f(sltiu) f(add) f(sub) \
 f(inv) f(nemu_trap)
 
--- nemu/src/isa/riscv32/instr/branch.h
+++ nemu/src/isa/riscv32/instr/branch.h
@@ -2,3 +2,8 @@ def_EHelper(beq){
   if (id_src1->imm == id_src2->imm)
   rtl_j(s, s->pc + id_src1->imm);
 }
+
+def_EHelper(bne){
+  if (id_src1->imm != id_src2->imm)
+  rtl_j(s, s->pc + id_src1->imm);
+}
