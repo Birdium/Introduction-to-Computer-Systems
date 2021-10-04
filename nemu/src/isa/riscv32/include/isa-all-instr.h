fcc4ffc2a17d6e3a9bd9b300f69cbcf170d272bc
tracer-ics2021 <tracer@njuics.org>
2021-10-04 17:27:03 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: b2e138986f484b37b445a1d9cb629c05   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  17:27:03 up 20:13,  1 user,  load average: 1.00, 0.78, 0.57

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -2,7 +2,7 @@
 #include "../local-include/rtl.h"
 
 #define INSTR_LIST(f) f(lui) f(auipc) f(jal) f(jalr) \
-f(beq) f(bne) f(blt) f(bge) \
+f(beq) f(bne) f(blt) f(bge) f(bltu) f(bgeu) \
 f(lb) f(lh) f(lw) f(lbu) f(lhu) \
 f(sb) f(sh) f(sw) \
 f(addi) f(slti) f(sltiu) f(xori) f(ori) f(andi) f(slli) f(srli) f(srai) \
--- nemu/src/isa/riscv32/instr/branch.h
+++ nemu/src/isa/riscv32/instr/branch.h
@@ -22,3 +22,13 @@ def_EHelper(bge){
   if ((sword_t)*dsrc1 >= (sword_t)*dsrc2)
     rtl_j(s, s->pc + id_dest->imm);
 }
+
+def_EHelper(bltu){
+  if (*dsrc1 < *dsrc2)
+    rtl_j(s, s->pc + id_dest->imm);
+}
+
+def_EHelper(bgeu){
+  if (*dsrc1 >= *dsrc2)
+    rtl_j(s, s->pc + id_dest->imm);
+}
