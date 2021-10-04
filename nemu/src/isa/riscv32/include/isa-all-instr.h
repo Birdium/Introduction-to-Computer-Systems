661313421812699fcf49df8b7a78bf225c5a0dc8
tracer-ics2021 <tracer@njuics.org>
2021-10-04 13:05:49 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: b2e138986f484b37b445a1d9cb629c05   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  13:05:49 up 15:52,  1 user,  load average: 0.45, 0.43, 0.34

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -2,7 +2,7 @@
 #include "../local-include/rtl.h"
 
 #define INSTR_LIST(f) f(lui) f(auipc) f(jal) f(jalr) \
-f(beq) f(bne) f(blt) f(bge) f(lb) f(lh) f(lw) f(sw) \
+f(beq) f(bne) f(blt) f(bge) f(lb) f(lh) f(lw) f(lbu) f(lhu) f(sw) \
 f(addi) f(slti) f(sltiu) f(srli) f(srai) f(add) f(sub) f(srl) f(sra)\
 f(inv) f(nemu_trap)
 
--- nemu/src/isa/riscv32/instr/ldst.h
+++ nemu/src/isa/riscv32/instr/ldst.h
@@ -12,6 +12,16 @@ def_EHelper(lw) {
   rtl_lm(s, ddest, dsrc1, id_src2->imm, 4);
 }
 
+def_EHelper(lbu) {
+  rtl_lm(s, s0, dsrc1, id_src2->imm, 1);
+  rtl_zext(s, ddest, s0, 1);
+}
+
+def_EHelper(lhu) {
+  rtl_lm(s, s0, dsrc1, id_src2->imm, 2);
+  rtl_zext(s, ddest, s0, 2);
+}
+
 def_EHelper(sw) {
   rtl_sm(s, ddest, dsrc1, id_src2->imm, 4);
 }
