adf24689b72050855ecc32f1d048fca051d65659
tracer-ics2021 <tracer@njuics.org>
2021-10-04 11:37:10 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: b2e138986f484b37b445a1d9cb629c05   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  11:37:10 up 14:23,  1 user,  load average: 0.45, 0.39, 0.43

--- nemu/src/isa/riscv32/instr/branch.h
+++ nemu/src/isa/riscv32/instr/branch.h
@@ -1,14 +1,14 @@
 //test
-#include<unistd.h>
+//#include<unistd.h>
 
 def_EHelper(beq){
-  if (id_src1->imm == id_src2->imm)
+  if (*dsrc1 == *dsrc2)
     rtl_j(s, s->pc + id_dest->imm);
 }
 
 def_EHelper(bne){
-  if (id_src1->imm != id_src2->imm)
+  if (*dsrc1 == *dsrc2)
     rtl_j(s, s->pc + id_dest->imm);
-  printf("0x%08x != 0x%08x\n", id_src1->imm, id_src2->imm);  
-  sleep(2);
+  //printf("0x%08x != 0x%08x\n", id_src1->imm, id_src2->imm);  
+  //sleep(2);
 }
