db06a22de54e22068265a8153dd92e894e12027b
tracer-ics2021 <tracer@njuics.org>
2021-10-07 21:37:29 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 2ed44253d36b4e30a9ab9f9e60dd83ab   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  21:37:29 up  1:08,  1 user,  load average: 0.35, 0.42, 0.36

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,15 +14,68 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  vsprintf(out, fmt, ap);
+  va_end(ap);
+  return 0;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  vsnprintf(out, n, fmt, ap);
+  va_end(ap);
+  return 0;
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  char *op = out; const char *fp = fmt;
+  if (n == 0) return 0;
+  do {
+    if (*fp == '%'){
+      fp++;
+      switch (*fp){
+      case 'd': 
+        {
+          int arg = va_arg(ap, int);
+          if (arg < 0) {
+            *op = '-';
+            ++op; --n;
+          }
+          int tmp = arg; size_t len = 0;  
+          while (tmp != 0) {tmp /= 10; ++len;} // pre-calc length
+          while (len > n) {arg /= 10; --len;} // cut end
+          op += len; n -= len;
+          if (len){
+            char *ap = op - 1;
+            while (arg != 0){
+              *ap = arg % 10;
+              arg /= 10;
+              ap--;
+            }
+          }
+        }
+        break;
+      case 's': 
+        {
+          char *arg = va_arg(ap, char*);
+          while (*arg != '\0' && n != 0){
+            *op = *arg;
+            ++op; --n;
+          }
+        }
+        break;
+      default:
+        assert(0);
+      }
+    }
+    else {
+      *op = *fp;
+      ++op; ++fp; --n;
+    }
+  } while(*fp != '\0' && n != 0);
+  return 0;
 }
 
 #endif
