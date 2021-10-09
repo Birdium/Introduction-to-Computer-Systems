31e8e98b93595fcc8698cb81276849c5d912e1d8
tracer-ics2021 <tracer@njuics.org>
2021-10-09 12:27:34 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: f0f02339a6f04ab28d22004877c8d837   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  12:27:34 up 9 min,  1 user,  load average: 0.44, 0.52, 0.37

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -33,14 +33,14 @@ $(OBJ_DIR)/%.o: %.c
 	@echo + CC $<
 	@mkdir -p $(dir $@)
 	@$(CC) $(CFLAGS) -c -o $@ $<
-	@$(CC) $(CFLAGS) -E -o $@.i $<
+	@$(CC) $(CFLAGS) -E -o $(basename $@).i $<
 	$(call call_fixdep, $(@:.o=.d), $@)
 
 $(OBJ_DIR)/%.o: %.cc
 	@echo + CXX $<
 	@mkdir -p $(dir $@)
 	@$(CXX) $(CFLAGS) $(CXXFLAGS) -c -o $@ $<
-	@$(CXX) $(CFLAGS) $(CXXFLAGS) -E -o $@.i $<
+	@$(CXX) $(CFLAGS) $(CXXFLAGS) -E -o $(basename $@).i $<
 	$(call call_fixdep, $(@:.o=.d), $@)
 
 # Depencies
