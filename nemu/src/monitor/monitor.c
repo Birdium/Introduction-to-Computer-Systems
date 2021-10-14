b352758c2144044354fd1c7df76434cfd184a6ac
tracer-ics2021 <tracer@njuics.org>
2021-10-14 23:20:56 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 845b233916324f3ab64bfad17d0e1898   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  23:20:56 up  1:08,  1 user,  load average: 0.29, 0.44, 0.37

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -35,6 +35,7 @@ void ftrace_call(vaddr_t pc, vaddr_t dest){
   log_write(FMT_WORD ": ", pc);
   for(int i = 0; i < recursion_depth; i++) log_write(" ");
   for(int i = 0; i < sym_num; i++){
+    printf("%d" FMT_WORD FMT_WORD "\n", i, sym[i].st_value, sym[i].st_size);
     if (sym[i].st_info == STT_FUNC){
       if (sym[i].st_value <= dest && dest < sym[i].st_value + sym[i].st_size){
         char *func_name = strtab + sym[i].st_name;
