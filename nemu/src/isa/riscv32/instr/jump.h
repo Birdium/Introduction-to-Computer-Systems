248b122d08c5cc746cd365f33672e3f5ac5f4b67
tracer-ics2021 <tracer@njuics.org>
2021-10-14 23:30:42 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 845b233916324f3ab64bfad17d0e1898   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  23:30:42 up  1:18,  1 user,  load average: 0.95, 0.73, 0.54

--- nemu/src/isa/riscv32/instr/jump.h
+++ nemu/src/isa/riscv32/instr/jump.h
@@ -13,12 +13,12 @@ def_EHelper(jal){
 }
 
 def_EHelper(jalr){
-#ifdef CONFIG_FTRACE
-  if (id_src1->imm == 8 && id_src2->imm == 0) 
-    ftrace_ret(s->pc, s->pc + id_src1->imm);
-  else 
-    ftrace_call(s->pc, s->pc + id_src1->imm);
-#endif
+// #ifdef CONFIG_FTRACE
+//   if (id_src1->imm == 8 && id_src2->imm == 0) 
+//     ftrace_ret(s->pc, s->pc + id_src1->imm);
+//   else 
+//     ftrace_call(s->pc, s->pc + id_src1->imm);
+// #endif
   rtl_addi(s, s0, dsrc1, id_src2->imm);
   rtl_jr(s, s0);
   rtl_li(s, ddest, s->pc + 4);
