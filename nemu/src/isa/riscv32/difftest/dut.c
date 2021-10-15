9eab04a9ff8a0bd170bb902d634cf82a574d4294
tracer-ics2021 <tracer@njuics.org>
2021-10-15 14:57:32 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 05587dc575f54c618c06d875eaf06e99   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  14:57:32 up  1:04,  1 user,  load average: 1.45, 1.50, 1.31

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -3,8 +3,8 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
+  printf(FMT_WORD " " FMT_WORD "\n", cpu.pc, pc);
   if (cpu.pc != pc) return false;
-  assert(0);
   for(int i = 0; i < 31; i++){
     if (cpu.gpr[i]._32 == ref_r->gpr[i]._32) return false;
   }
