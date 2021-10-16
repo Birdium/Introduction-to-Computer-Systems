8cd0cca51ba06d2104c290f99f5e90afa87da232
tracer-ics2021 <tracer@njuics.org>
2021-10-16 19:48:50 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 6b51233489bd4182a66ed1952077f3aa   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  19:48:50 up  5:32,  1 user,  load average: 0.97, 0.56, 0.42

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -43,18 +43,66 @@ int snprintf(char *out, size_t n, const char *fmt, ...) {
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
   size_t ch_num = 0;
   char *op = out; const char *fp = fmt;
+        
   while(*fp != '\0' && ch_num < n) {
     if (*fp == '%'){
       ++fp;
+      enum {no_fill, left_fill, right_fill} fill_type = no_fill;
+      char fill_ch = ' ';
+      size_t width = 0; // precision = 0;
+      // flag_characters
+      bool flag = 1;
+      while (flag){
+        switch(*fp){
+          case '-': 
+            fill_type = right_fill;
+            break;
+          case '0':
+            if (fill_type == no_fill) fill_type = left_fill;
+            fill_ch = '0';
+            break;
+          default:
+            flag = 0;
+            --fp;
+            break;
+        }
+        if (flag) ++fp;
+      }
+      // field width
+      flag = 1;
+      while (flag){
+        switch(*fp){
+          case'0': case'1': case'2': case'3': case'4': case'5': case'6': case'7': case'8': case'9':
+            width = width * 10 + *fp - '0';
+            break;
+          default:
+            flag = 0;
+            break;
+        }
+        if (flag) ++fp;
+      }
       switch (*fp){
       case 'd': 
         {
           int arg = va_arg(ap, int);
           itoa(arg, buf, 10);
           char *bp = buf;
+          size_t buf_len = strlen(buf);
+          if (fill_type == left_fill){
+            while(buf_len < width && ch_num < n){
+              *op = fill_ch;
+              ++op; ++ch_num; width--;
+            }
+          }
           while(*bp != '\0' && ch_num < n){
             *op = *bp;
             ++op; ++ch_num; ++bp;
+          }
+          if (fill_type == right_fill){
+            while(buf_len < width && ch_num < n){
+              *op = fill_ch;
+              ++op; ++ch_num; width--;
+            }
           }
         }
         break;
