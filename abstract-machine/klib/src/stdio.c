5c309b799266f29deedf1553f59d77db17fcdda0
tracer-ics2021 <tracer@njuics.org>
2021-10-16 22:45:05 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 6b51233489bd4182a66ed1952077f3aa   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  22:45:05 up  8:28,  1 user,  load average: 0.74, 0.37, 0.28

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -8,11 +8,12 @@
 static char buf[8192];
 
 int printf(const char *fmt, ...) {
+  char p_buf[8192];
   va_list ap;
   va_start(ap, fmt);
-  int ret = vsprintf(buf, fmt, ap);
+  int ret = vsprintf(p_buf, fmt, ap);
   va_end(ap);
-  char *bp = buf;
+  char *bp = p_buf;
   while(*bp != '\0') {
     putch(*bp); ++bp;
   }
@@ -54,16 +55,12 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
       bool flag = 1;
       while (flag){
         switch(*fp){
-          case '-': 
-            fill_type = right_fill;
-            break;
-          case '0':
+          case '-': fill_type = right_fill; break;
+          case '0': 
             if (fill_type == no_fill) fill_type = left_fill;
             fill_ch = '0';
             break;
-          default:
-            flag = 0;
-            break;
+          default: flag = 0; break;
         }
         if (flag) ++fp;
       }
@@ -72,11 +69,8 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
       while (flag){
         switch(*fp){
           case'0': case'1': case'2': case'3': case'4': case'5': case'6': case'7': case'8': case'9':
-            width = width * 10 + *fp - '0';
-            break;
-          default:
-            flag = 0;
-            break;
+            width = width * 10 + *fp - '0'; break;
+          default: flag = 0; break;
         }
         if (flag) ++fp;
       }
