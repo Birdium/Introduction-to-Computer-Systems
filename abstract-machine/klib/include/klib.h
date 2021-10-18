ae8d5eadcbd0d0b7f858f5bf0f7c8da89bf93407
tracer-ics2021 <tracer@njuics.org>
2021-10-18 18:31:57 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: e8e71aeb4b824b32b8a24c85d2089660   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  18:31:57 up  2:10,  1 user,  load average: 0.34, 0.36, 0.31

--- abstract-machine/klib/include/klib.h
+++ abstract-machine/klib/include/klib.h
@@ -4,6 +4,7 @@
 #include <am.h>
 #include <stddef.h>
 #include <stdarg.h>
+#include <stdio.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -34,11 +35,11 @@ void   itoa      (int val, char *dest, int base);
 void   uitoa     (unsigned uval, char *dest, int base);
 
 // stdio.h
-int    printf    (const char *format, ...);
-int    sprintf   (char *str, const char *format, ...);
-int    snprintf  (char *str, size_t size, const char *format, ...);
-int    vsprintf  (char *str, const char *format, va_list ap);
-int    vsnprintf (char *str, size_t size, const char *format, va_list ap);
+// int    printf    (const char *format, ...);
+// int    sprintf   (char *str, const char *format, ...);
+// int    snprintf  (char *str, size_t size, const char *format, ...);
+// int    vsprintf  (char *str, const char *format, va_list ap);
+// int    vsnprintf (char *str, size_t size, const char *format, va_list ap);
 
 // assert.h
 #ifdef NDEBUG
