2b6048af560c3802e5eb0750c18ee29cd34dc570
tracer-ics2021 <tracer@njuics.org>
2021-10-18 19:54:55 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: e8e71aeb4b824b32b8a24c85d2089660   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  19:54:55 up  3:33,  1 user,  load average: 0.35, 0.32, 0.33

--- nemu/src/utils/timer.c
+++ nemu/src/utils/timer.c
@@ -28,7 +28,7 @@ static bool is_time_inited = 0;
 uint64_t get_time() {
   if (!is_time_inited){
     boot_time = get_time_internal();
-    printf("%lu", boot_time);
+    printf("%lu\n", boot_time);
     is_time_inited = 1;
   }
   uint64_t now = get_time_internal();
