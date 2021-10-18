16f52084a735fc1102ce7d46bb5345b27b7b384e
tracer-ics2021 <tracer@njuics.org>
2021-10-18 19:29:28 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: e8e71aeb4b824b32b8a24c85d2089660   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  19:29:28 up  3:08,  1 user,  load average: 0.62, 0.50, 0.33

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -2,15 +2,16 @@
 #include <nemu.h>
 //#include <stdio.h>
 
-static uint32_t lo = 0, hi = 0;
+static uint64_t boot_time = 0;
 
 void __am_timer_init() {
+  boot_time = inl(RTC_ADDR + 4) * 1000000 + inl(RTC_ADDR + 0);
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  lo = inl(RTC_ADDR + 0);
-  hi = inl(RTC_ADDR + 4);
-  uptime->us = (((uint64_t)hi * 1000000) + lo + 500); 
+  uint64_t lo = inl(RTC_ADDR + 0);
+  uint64_t hi = inl(RTC_ADDR + 4);
+  uptime->us = hi * 1000000 + lo - boot_time; 
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
