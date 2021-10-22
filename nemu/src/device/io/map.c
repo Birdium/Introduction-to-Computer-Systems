678c72605bdc8315be6301abeb33a0514a9a5e19
tracer-ics2021 <tracer@njuics.org>
2021-10-22 11:38:20 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 4efdbde6cfb64829bc21f8dd3d15c8cd   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  11:38:20 up  1:30,  1 user,  load average: 0.45, 0.50, 0.51

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -42,7 +42,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
 #ifdef CONFIG_DTRACE
-  log_write("Device : read %s at " FMT_WORD, map->name, addr);
+  log_write("Device : read %s at " FMT_WORD "\n", map->name, addr);
 #endif 
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
@@ -55,7 +55,7 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
 #ifdef CONFIG_DTRACE
-  log_write("Device : write %s at " FMT_WORD, map->name, addr);
+  log_write("Device : write %s at " FMT_WORD "\n", map->name, addr);
 #endif
   invoke_callback(map->callback, offset, len, true);
 }
