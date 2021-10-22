32c0b7468b6b0b4195899f3c021257db97731094
tracer-ics2021 <tracer@njuics.org>
2021-10-22 13:34:58 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: a48c2558242549dea9f84251d757b19b   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  13:34:58 up 41 min,  1 user,  load average: 0.44, 0.36, 0.34

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -23,7 +23,7 @@ void __am_gpu_init() {
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = inl(VGACTL_ADDR), .height = inl(VGACTL_ADDR + 2),
+    .width = inw(VGACTL_ADDR), .height = inw(VGACTL_ADDR + 2),
     .vmemsz = 0
   };
 }
