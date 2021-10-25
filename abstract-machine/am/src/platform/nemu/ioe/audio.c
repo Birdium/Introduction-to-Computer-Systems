de3128e155fd1f5f74ccf500fd2292914bd4f35b
tracer-ics2021 <tracer@njuics.org>
2021-10-25 16:26:59 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 2a69da8c9bc44affaf68fe6c3435a5f0   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  16:26:59 up 14 min,  1 user,  load average: 0.47, 0.40, 0.26

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -1,6 +1,6 @@
 #include <am.h>
 #include <nemu.h>
-//#include <SDL2/SDL_audio.h>
+// #include <SDL2/SDL_audio.h>
 
 #define AUDIO_FREQ_ADDR      (AUDIO_ADDR + 0x00)
 #define AUDIO_CHANNELS_ADDR  (AUDIO_ADDR + 0x04)
@@ -9,30 +9,12 @@
 #define AUDIO_INIT_ADDR      (AUDIO_ADDR + 0x10)
 #define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
 
-static int head = 0, tail = 0;
+static int head = 0;
 static int bufnum = 0;
-
-// static void audio_play(void *userdata, uint8_t *stream, int len) {
-//   int nread = len;
-//   volatile int cnt = inl(AUDIO_COUNT_ADDR);
-//   if (cnt < len) nread = cnt;
-//   uint8_t *buf  = AUDIO_SBUF_ADDR;
-//   for(int i = 0; i < len; i++){
-//     assert(head != tail);
-//     stream[i] = inb(buf + tail);
-//     tail++; if (tail == bufnum) tail = 0;
-//   }
-//   cnt -= nread;
-//   outl(AUDIO_COUNT_ADDR, cnt);
-//   if (len > nread) {
-//     memset(stream + nread, 0, len - nread);
-//   }
-// }
 
 static void audio_write(uint8_t *buf, int len) {
   uint8_t *sbuf = (uint8_t *) AUDIO_SBUF_ADDR;
   for (int i = 0; i < len; i++) {
-    assert(head != tail - 1);
     outb((uint32_t)sbuf + i, buf[i + head]);
     head++; if (head >= bufnum) head = 0;
   }
@@ -54,20 +36,6 @@ void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
   outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
   outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
   outl(AUDIO_INIT_ADDR, 1);
-
-  // SDL_AudioSpec s = {};
-  // s.freq = ctrl->freq;
-  // s.format = AUDIO_S16SYS;
-  // s.channels = ctrl->channels;
-  // s.samples = ctrl->samples;
-  // s.callback = audio_play;
-  // s.userdata = NULL;
-
-  // int ret = SDL_InitSubSystem(SDL_INIT_AUDIO);
-  // if (ret == 0) {
-  //   SDL_OpenAudio(&s, NULL);
-  //   SDL_PauseAudio(0);
-  // }
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -1,5 +1,6 @@
 #include <common.h>
 #include <device/map.h>
+#include <SDL2/SDL.h>
 enum {
   reg_freq,
   reg_channels,
@@ -10,8 +11,28 @@ enum {
   nr_reg
 };
 
+#define SBUF_NUM CONFIG_SB_SIZE / sizeof(uint8_t)
+
 static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
+SDL_AudioSpec s = {};
+
+static int tail = 0;
+
+static void audio_play(void *userdata, uint8_t *stream, int len) {
+  int nread = len;
+  volatile int cnt = audio_base[reg_count];
+  if (cnt < len) nread = cnt;
+  for(int i = 0; i < len; i++){
+    stream[i] = sbuf[i + tail];
+    tail++; if (tail == SBUF_NUM) tail = 0;
+  }
+  cnt -= nread;
+  audio_base[reg_count] = cnt;
+  if (len > nread) {
+    memset(stream + nread, 0, len - nread);
+  }
+}
 
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
   switch (offset){
@@ -20,7 +41,18 @@ static void audio_io_handler(uint32_t offset, int len, bool is_write) {
       audio_base[reg_sbuf_size] = CONFIG_SB_SIZE;
       break;
     case 16: 
-      
+      s.freq = audio_base[reg_freq];
+      s.format = AUDIO_S16SYS;
+      s.channels = audio_base[reg_channels];
+      s.samples = audio_base[reg_samples];
+      s.callback = audio_play;
+      s.userdata = NULL;
+
+      int ret = SDL_InitSubSystem(SDL_INIT_AUDIO);
+      if (ret == 0) {
+        SDL_OpenAudio(&s, NULL);
+        SDL_PauseAudio(0);
+      }
       break;
     case 20: break;
     default: assert(0);
