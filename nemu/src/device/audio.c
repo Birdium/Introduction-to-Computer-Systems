9d03a2b11bd39dfb3126475f13f2c2290848ce0a
tracer-ics2021 <tracer@njuics.org>
2021-10-25 16:31:14 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 2a69da8c9bc44affaf68fe6c3435a5f0   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  16:31:14 up 18 min,  1 user,  load average: 0.69, 0.45, 0.31

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -15,9 +15,8 @@ enum {
 
 static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
-SDL_AudioSpec s = {};
 
-static int tail = 0;
+static int tail = 0, cnt = 0;
 
 static void audio_play(void *userdata, uint8_t *stream, int len) {
   int nread = len;
@@ -40,21 +39,24 @@ static void audio_io_handler(uint32_t offset, int len, bool is_write) {
     case 12:
       audio_base[reg_sbuf_size] = CONFIG_SB_SIZE;
       break;
-    case 16: 
+    case 16: ;  
+      SDL_AudioSpec s = {};
       s.freq = audio_base[reg_freq];
       s.format = AUDIO_S16SYS;
       s.channels = audio_base[reg_channels];
       s.samples = audio_base[reg_samples];
       s.callback = audio_play;
       s.userdata = NULL;
-
       int ret = SDL_InitSubSystem(SDL_INIT_AUDIO);
       if (ret == 0) {
         SDL_OpenAudio(&s, NULL);
         SDL_PauseAudio(0);
       }
       break;
-    case 20: break;
+    case 20: 
+      if (is_write) cnt = audio_base[reg_count];
+      else audio_base[reg_count] = cnt;
+      break;
     default: assert(0);
   }
 }
