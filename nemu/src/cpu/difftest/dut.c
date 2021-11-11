f444ddeb2b3c397aeab2180eb5db732f8c759284
tracer-ics2021 <tracer@njuics.org>
2021-11-11 21:48:42 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 8b0f2c3f452e444395bab082af4ea348   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  21:48:42 up  4:00,  1 user,  load average: 0.90, 0.57, 0.41

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -76,11 +76,27 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
   ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
 }
 
+const char *regs[] = {
+  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+};
+
+static void isa_ref_reg_display(CPU_state *ref){
+  int i;
+	for(i = 0; i < 32; i++){
+		printf("%-16s0x%-16x%-16u\n", regs[i], ref->gpr[i]._32, ref->gpr[i]._32);
+	}
+}
+
 static void checkregs(CPU_state *ref, vaddr_t pc) {
   if (!isa_difftest_checkregs(ref, pc)) {
+    printf("Program failed difftest.\n");
     nemu_state.state = NEMU_ABORT;
     nemu_state.halt_pc = pc;
     isa_reg_display();
+    isa_ref_reg_display(ref);
   }
 }
 
