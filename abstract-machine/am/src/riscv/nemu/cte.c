9721370818581b0924cc52a5ed8193708bf80c2a
tracer-ics2021 <tracer@njuics.org>
2021-11-16 23:46:02 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: d164df24b7e1441fac0e7dec27f0cfcc   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  23:46:02 up  2:31,  1 user,  load average: 0.50, 0.67, 0.58

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -17,6 +17,7 @@ Context* __am_irq_handle(Context *c) {
     Event ev = {0};
     switch (c->mcause) {
       case 0xb :{
+          printf("0x%x\n", c->gpr[17]);
         switch (c->gpr[17]) {
           case 0xffffffff : ev.event = EVENT_YIELD; break;
           default: ev.event = EVENT_ERROR; break;
