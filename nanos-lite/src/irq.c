fa5c91798d8c6a8b959fe4ed3b37a4c979e1aa00
tracer-ics2021 <tracer@njuics.org>
2021-11-16 21:28:26 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: d164df24b7e1441fac0e7dec27f0cfcc   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  21:28:26 up 13 min,  1 user,  load average: 0.47, 0.38, 0.34

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,8 +4,7 @@ static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD : 
       printf("Yielded...\n");
-      int tmp = 0;
-      asm volatile("csrr %0, mepc; addi %0, %0, 4; csrw mepc, %0" : : "r"(tmp));
+      c->mepc += 4;
       break;
     default: panic("Unhandled event ID = %d", e.event);
   }
