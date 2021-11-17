460a7dd0660765cc53ab80edd03cd5019d4041b5
tracer-ics2021 <tracer@njuics.org>
2021-11-17 10:14:38 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 4409900d52784bd595a076ac658d580a   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  10:14:38 up 14 min,  1 user,  load average: 0.75, 1.12, 0.88

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,10 +1,20 @@
 #include <common.h>
 #include "syscall.h"
+
+enum {
+    SYS_EXIT = 0,
+    SYS_YIELD = 1
+} syscall_type;
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
   switch (a[0]) {
+    case SYS_YIELD: yield(); c->GPRx = 0; break;
+    //case SYS_EXIT: halt()
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
