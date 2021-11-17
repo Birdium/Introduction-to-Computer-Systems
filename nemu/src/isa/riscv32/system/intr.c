f7f546bc69d86c756a54784caca094f9d71c3b8d
tracer-ics2021 <tracer@njuics.org>
2021-11-17 12:53:41 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: a866d30a3c2b41edb5144b77e87d37e3   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  12:53:41 up 15 min,  1 user,  load average: 0.42, 0.41, 0.36

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -9,7 +9,7 @@ word_t isa_raise_intr(Decode *s, word_t NO, vaddr_t epc) {
   rtl_li(s, mcause, NO);
   rtl_jr(s, mtvec);
   #ifdef CONFIG_ETRACE
-  log_write("Exception NO: "FMT_WORD" in " FMT_WORD ", jump to "FMT_WORD, NO, epc, *mtvec);
+  log_write("Exception NO: "FMT_WORD" in " FMT_WORD ", jump to "FMT_WORD"\n", NO, epc, *mtvec);
   #endif
   return 0;
 }
