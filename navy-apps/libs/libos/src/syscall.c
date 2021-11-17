5adc4811a8b4ac657ed9f8d57adeb94db82e323b
tracer-ics2021 <tracer@njuics.org>
2021-11-17 13:22:28 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: a866d30a3c2b41edb5144b77e87d37e3   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  13:22:28 up 44 min,  1 user,  load average: 0.39, 0.36, 0.28

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -63,8 +63,7 @@ int _open(const char *path, int flags, mode_t mode) {
 int _write(int fd, void *buf, size_t count) {
   //printf("%d 0x%p 0x%x\n", fd, buf, count);
   // assert(count != 1); 
-  _syscall_(SYS_write, fd, buf, count);
-  return 0;
+  return _syscall_(SYS_write, fd, buf, count);
 }
 
 void *_sbrk(intptr_t increment) {
