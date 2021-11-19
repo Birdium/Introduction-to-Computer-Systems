4dffc8a7c33506668f2c8006df6ab6748f45308e
tracer-ics2021 <tracer@njuics.org>
2021-11-19 12:14:58 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 6abcc9cfe13c4e0190abe476d952bd05   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  12:14:58 up  1:07,  1 user,  load average: 0.55, 0.61, 0.52

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -37,8 +37,8 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
 }
 
 int sys_gettimeofday(struct timeval *tv, struct timezone *tz) {
-  tv->tv_sec = io_read(AM_TIMER_UPTIME).us / 1000000;
   tv->tv_usec = io_read(AM_TIMER_UPTIME).us % 1000000;
+  tv->tv_sec = io_read(AM_TIMER_UPTIME).us / 1000000;
   return 0;
 }
 
