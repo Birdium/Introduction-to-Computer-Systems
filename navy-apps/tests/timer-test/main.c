5ceb074a56b417728d62c2a079e99c4444497c10
tracer-ics2021 <tracer@njuics.org>
2021-11-19 12:09:07 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 6abcc9cfe13c4e0190abe476d952bd05   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  12:09:07 up  1:01,  1 user,  load average: 0.58, 0.51, 0.45

---
+++ navy-apps/tests/timer-test/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <assert.h>
+#include <sys/time.h>
+
+int main() {
+
+  struct timeval start;
+  gettimeofday(&start, NULL);  
+  int half_sec = 0;
+  while (1) {
+    struct timeval now;
+    gettimeofday(&now, NULL);
+    if (2 * (now.tv_sec - start.tv_sec) + (now.tv_usec - start.tv_usec) / 500000 > half_sec){
+        printf("%d.%d seconds).\n", half_sec / 2, half_sec % 2 * 5);
+    } 
+  }
+  return 0;
+}
