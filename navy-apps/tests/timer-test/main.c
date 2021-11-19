2edfe66e1483dbde445d3c743f50d5e3fdcb0a96
tracer-ics2021 <tracer@njuics.org>
2021-11-19 14:17:11 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 01e7da1a4f3949b4b50de6270fc04f06   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  14:17:11 up 18 min,  1 user,  load average: 0.77, 0.78, 0.61

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <assert.h>
 #include <sys/time.h>
+
+uint32_t NDL_GetTicks();
 struct timeval start;
 struct timeval now;
 
 int main() {
 
-  gettimeofday(&start, NULL);  
+  uint32_t start = NDL_GetTicks();
   int half_sec = 0;
   while (1) {
-    gettimeofday(&now, NULL);
-    printf("%d:%d\n", (int)now.tv_sec, (int)now.tv_usec);
-    // if (((now.tv_sec - sta1rt.tv_sec) * 1000000 + now.tv_usec - start.tv_usec) / 500000 > half_sec){
-    //     // printf("%d / 2 seconds).\n", half_sec);
-    //     half_sec ++;
-    // } 
+    uint32_t now = NDL_GetTicks();
+    // printf("%d:%d\n", (int)now.tv_sec, (int)now.tv_usec);
+    if ((now - start) / 500 > half_sec){
+        printf("%d / 2 seconds).\n", half_sec);
+        half_sec ++;
+    } 
   }
   return 0;
 }
