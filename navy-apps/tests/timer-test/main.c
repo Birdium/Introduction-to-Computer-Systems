78b01656f64fc352a99d5de452ea5a33df78de94
tracer-ics2021 <tracer@njuics.org>
2021-11-19 12:10:42 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 6abcc9cfe13c4e0190abe476d952bd05   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  12:10:42 up  1:02,  1 user,  load average: 0.66, 0.53, 0.46

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -12,6 +12,7 @@ int main() {
     gettimeofday(&now, NULL);
     if (2 * (now.tv_sec - start.tv_sec) + (now.tv_usec - start.tv_usec) / 500000 > half_sec){
         printf("%d.%d seconds).\n", half_sec / 2, half_sec % 2 * 5);
+        half_sec ++;
     } 
   }
   return 0;
