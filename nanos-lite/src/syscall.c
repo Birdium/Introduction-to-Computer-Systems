9c2407418e3ab32d0ee541ec2f67cfdbce431dc7
tracer-ics2021 <tracer@njuics.org>
2021-11-19 11:17:05 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 6abcc9cfe13c4e0190abe476d952bd05   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  11:17:05 up 9 min,  1 user,  load average: 0.53, 0.47, 0.31

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -9,20 +9,20 @@ static uintptr_t sys_brk(void *addr){
   return 0;
 }
 
-size_t sys_write(int fd, char *buf, size_t count){
-  // printf("0x%x\n", count);
-  uintptr_t ret = 0;
-  if (fd == 1 || fd == 2){
-    //printf("%d\n", count);
-    for(size_t i = 0; i < count; i++){
-      putch(*(buf + i));
-      ret++;
-    }
-  } else {
-    ret = fs_write(fd, buf, count);
-  }
-  return ret;
-}
+// size_t sys_write(int fd, char *buf, size_t count){
+//   // printf("0x%x\n", count);
+//   uintptr_t ret = 0;
+//   if (fd == 1 || fd == 2){
+//     //printf("%d\n", count);
+//     for(size_t i = 0; i < count; i++){
+//       putch(*(buf + i));
+//       ret++;
+//     }
+//   } else {
+//     ret = fs_write(fd, buf, count);
+//   }
+//   return ret;
+// }
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
@@ -38,7 +38,7 @@ void do_syscall(Context *c) {
     case SYS_exit: halt(a[1]); break;
     case SYS_open: c->GPRx = fs_open((const char*)a[1], a[2], a[3]); break; 
     case SYS_read: c->GPRx = fs_read(a[1], (void*)a[2], a[3]); break;
-    case SYS_write: c->GPRx = sys_write(a[1], (void*)a[2], a[3]); break;
+    case SYS_write: c->GPRx = fs_write(a[1], (void*)a[2], a[3]); break;
     case SYS_close: c->GPRx = fs_close(a[1]); break;
     case SYS_lseek: c->GPRx = fs_lseek(a[1], a[2], a[3]); break;
     case SYS_brk: c->GPRx = sys_brk((void*)a[1]); break;
