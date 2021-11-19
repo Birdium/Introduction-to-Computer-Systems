067a0d410b1e5046b265ba8b756da2519a67324a
tracer-ics2021 <tracer@njuics.org>
2021-11-19 13:15:39 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 6abcc9cfe13c4e0190abe476d952bd05   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  13:15:39 up  2:07,  1 user,  load average: 1.28, 0.71, 0.49

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -10,7 +10,7 @@ int main() {
   int half_sec = 0;
   while (1) {
     gettimeofday(&now, NULL);
-    printf("%d:%d\n", (int)now.tv_sec, (int)now.tv_usec);
+    // printf("%d:%d\n", (int)now.tv_sec, (int)now.tv_usec);
     if (((now.tv_sec - start.tv_sec) * 1000000 + now.tv_usec - start.tv_usec) / 500000 > half_sec){
         printf("%d / 2 seconds).\n", half_sec);
         half_sec ++;
