38ac5936f7ae24628129f2c70b00a6dd314ec7bf
tracer-ics2021 <tracer@njuics.org>
2021-11-21 00:00:35 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 82d4275953c54171a73d0a53398bea81   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  00:00:35 up  1:29,  1 user,  load average: 0.56, 0.51, 0.48

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -51,8 +51,8 @@ void NDL_OpenCanvas(int *w, int *h) {
     *w = screen_w, *h = screen_h;
   }
   canvas_w = *w, canvas_h = *h;
-  canvas_x = (screen_w - canvas_w) / 2;
-  canvas_y = (screen_h - canvas_h) / 2;
+  // canvas_x = (screen_w - canvas_w) / 2;
+  // canvas_y = (screen_h - canvas_h) / 2;
   close(f_dispinfo);
 }
 
