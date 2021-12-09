d399375f1fb8b47a7018bbf67afc161ff2790e35
tracer-ics2021 <tracer@njuics.org>
2021-12-09 19:00:47 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 24a3ac184bbe4d47accb8376c79e9de9   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  19:00:47 up 28 min,  1 user,  load average: 0.46, 0.40, 0.40

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -52,5 +52,6 @@ void naive_uload(PCB *pcb, const char *filename) {
 void context_kload(PCB *pcb, void (*entry)(void *), void *arg) {
   pcb->cp = kcontext(RANGE(pcb->stack, pcb->stack + sizeof(pcb->stack)), entry, arg);
   printf("%p\n", pcb->cp);
-  printf("%x %x\n", pcb->stack, pcb->stack + sizeof(pcb->stack));
+  printf("%p\n", entry);
+  // printf("%x %x\n", pcb->stack, pcb->stack + sizeof(pcb->stack));
 }
