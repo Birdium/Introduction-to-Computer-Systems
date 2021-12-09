630628790d9c409a637e630e2763e6bacfc9dcaf
tracer-ics2021 <tracer@njuics.org>
2021-12-09 23:26:35 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: aa08a390c13d4c75ba2b76b47125e650   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  23:26:35 up 21 min,  1 user,  load average: 0.32, 0.62, 0.54

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -16,8 +16,8 @@ void hello_fun(void *arg) {
   int j = 1;
   while (1) {
     Log("Hello World from Nanos-lite with arg '%p' for the %dth time!", (uintptr_t)arg, j);
-    printf("%d\n", j);
     j ++;
+    printf("%d\n", j);
     yield();
   }
 }
