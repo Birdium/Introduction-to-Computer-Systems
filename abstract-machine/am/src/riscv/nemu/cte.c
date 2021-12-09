e0cf642fc2e45f970b30dbe07630f683fb76ccf5
tracer-ics2021 <tracer@njuics.org>
2021-12-09 23:45:09 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: aa08a390c13d4c75ba2b76b47125e650   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  23:45:09 up 40 min,  1 user,  load average: 0.37, 0.45, 0.53

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -60,7 +60,7 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  Context *cp = kstack.start; 
+  Context *cp = kstack.end - sizeof(Context); 
   cp->mstatus = 0x1800;
   cp->mepc = (uintptr_t)entry;
   printf("%x\n", cp->mepc);
