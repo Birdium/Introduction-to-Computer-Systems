8f8b0163065ce5e1187e41de07a0c33a6e23b0e3
tracer-ics2021 <tracer@njuics.org>
2021-12-10 20:02:18 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: a394cfca3162494cb01b18fd124d7aa2   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  20:02:18 up 30 min,  1 user,  load average: 0.41, 0.41, 0.44

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,8 +4,9 @@ void do_syscall(Context *c);
 Context* schedule(Context *prev);
 
 static Context* do_event(Event e, Context* c) {
+  // printf("%p\n", c);
   switch (e.event) {
-    case EVENT_YIELD : 
+    case EVENT_YIELD :
       printf("Yielded...\n");
       return schedule(c);
       break;
