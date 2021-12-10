60457a4e496af8889795cd21d909da3e42aecc71
tracer-ics2021 <tracer@njuics.org>
2021-12-10 15:23:55 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 9736776830444a82a1f34342c3dbf0a2   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  15:23:55 up  1:40,  1 user,  load average: 0.86, 0.98, 1.04

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -35,13 +35,13 @@ void itoa(int val, char* dest, int base){
   unsigned uval;
   bool flag = 0;
   if (val == -2147483648) {
-    assert(0);
     uval = (unsigned) 2147483648; flag = 1;
   }
   else if (val < 0){
     uval = (unsigned) -val; flag = 1;
   }
   else uval = val;
+  if (uval == 94) assert(0);
   if (uval){
     while(uval){
       *dp = index[uval % base];
