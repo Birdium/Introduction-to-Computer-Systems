61a0a7cad0d8301e73111db1a0f36766a8670178
tracer-ics2021 <tracer@njuics.org>
2021-12-13 14:20:17 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 435d83ba48474066ad177a2e181d9393   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  14:20:17 up 18 min,  1 user,  load average: 1.57, 1.40, 1.02

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -6,12 +6,12 @@ int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
   // char *empty[] =  {NULL };
+  assert(0);
   int argc = *args;
   char *argv = args + 1;
   char *envp = args + argc + 2;
   // printf("%p\n", (args + 1));
   environ = (char**)envp;
-  assert(0);
   exit(main(argc, (char**)argv, (char**)envp));
   assert(0);
 }
