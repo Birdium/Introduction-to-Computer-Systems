7e3c5eb8ed5ccf0b0324adf853a2cc057c184beb
tracer-ics2021 <tracer@njuics.org>
2021-12-13 21:06:40 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 7c6576ec6163468a91fa7539865883ab   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  21:06:40 up  4:25,  1 user,  load average: 1.01, 1.00, 0.92

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -28,9 +28,10 @@ void init_proc() {
   context_kload(&pcb[0], hello_fun, "114");
   // char *argv[] = {"--skip", NULL};
   // char *envp[] = {"114514"};
-  char *argv[] = {"/bin/exec-test", NULL};
+  char *uproc_name = "/bin/nterm"; 
+  char *argv[] = {uproc_name, NULL};
   char *envp[] = {NULL};
-  context_uload(&pcb[1], "/bin/exec-test", argv, envp);
+  context_uload(&pcb[1], uproc_name, argv, envp);
   switch_boot_pcb();
   Log("Initializing processes...");
 
