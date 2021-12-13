8b80f8a66edeafef5a4c8ccd3d2670b51806d259
tracer-ics2021 <tracer@njuics.org>
2021-12-13 18:00:36 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 7c6576ec6163468a91fa7539865883ab   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  18:00:36 up  1:19,  1 user,  load average: 0.74, 0.76, 0.81

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -57,7 +57,6 @@ void context_kload(PCB *pcb, void (*entry)(void *), void *arg) {
 }
 
 void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]) {
-  uintptr_t entry = loader(pcb, filename);
   // allocate memory
   void *ustack_start = new_page(8);
   Area ustack = {ustack_start, ustack_start + 8 * PGSIZE};
@@ -105,6 +104,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   *ap++ = 0;
   // printf("%d\n", *init_addr);
 
+  uintptr_t entry = loader(pcb, filename);
   pcb->cp = ucontext(&pcb->as, RANGE(pcb->stack, pcb->stack + sizeof(pcb->stack)), (void*)entry);
   pcb->cp->GPRx = (uintptr_t)init_addr;
   // printf("%x %x", heap.start, heap.end);
