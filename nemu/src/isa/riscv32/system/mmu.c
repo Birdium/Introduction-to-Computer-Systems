6eee3449dfdd5cc79d66100884ee6dcbbaaf5819
tracer-ics2021 <tracer@njuics.org>
2021-12-20 21:49:39 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: fa59298c88354722be83703ac5f192d9   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  21:49:39 up  1:24,  1 user,  load average: 1.67, 1.41, 1.18

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -14,7 +14,6 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
          table_ndx = (((uintptr_t)vaddr) >> 12) & 0x3ff; 
 
   paddr_t pg_dir_base = *satp & BASE_ADDR_MASK; // 0x3fffff originally, but we have 4GB only, the upper 2bit ignored.
-  printf("1111111\n");
 
   word_t pg_dir_entry = paddr_read(pg_dir_base + 4 * dir_ndx, 4);
   if ((pg_dir_entry & 0x1) == 0) { // V == 0, Invalid.
@@ -32,5 +31,6 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   // if (offset + len > PAGE_SIZE) return MEM_RET_CROSS_PAGE; // only need in CISC
   paddr_t pg_paddr = pg_table_entry & BASE_ADDR_MASK;
 
+  printf("1111111\n");
   return pg_paddr | MEM_RET_OK;
 }
