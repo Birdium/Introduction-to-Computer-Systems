c2b3120232e22a218c5840e14e17c7ff90110b4c
tracer-ics2021 <tracer@njuics.org>
2021-12-20 21:46:15 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: fa59298c88354722be83703ac5f192d9   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  21:46:15 up  1:21,  1 user,  load average: 1.54, 1.31, 1.12

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -13,7 +13,7 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
         //  offset = ((uintptr_t)vaddr) & 0xfff,
          table_ndx = (((uintptr_t)vaddr) >> 12) & 0x3ff; 
 
-  paddr_t pg_dir_base = *satp & 0xfffff; // 0x3fffff originally, but we have 4GB only, the upper 2bit ignored.
+  paddr_t pg_dir_base = *satp & BASE_ADDR_MASK; // 0x3fffff originally, but we have 4GB only, the upper 2bit ignored.
 
   word_t pg_dir_entry = paddr_read(pg_dir_base + 4 * dir_ndx, 4);
   if ((pg_dir_entry & 0x1) == 0) { // V == 0, Invalid.
@@ -21,7 +21,6 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
     return MEM_RET_FAIL;
   } 
   paddr_t pg_table_base = pg_dir_entry & BASE_ADDR_MASK;
-  assert(0);
 
   word_t pg_table_entry = paddr_read(pg_table_base + 4 * table_ndx, 4);
   if ((pg_dir_entry & 0x1) == 0) {
