148a4500c87d47926eb1b0a2d8bfc8ebf6ffefd5
tracer-ics2021 <tracer@njuics.org>
2021-12-20 23:59:45 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: e9fb916f264247688b5121f7a82e89e4   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  23:59:46 up  1:54,  1 user,  load average: 0.54, 0.67, 0.65

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -78,6 +78,7 @@ static uintptr_t loader(PCB *pcb, const char *filename)   {
       }
       // printf("%x %x\n", vaddr, maddr);
       assert(vaddr == maddr);
+      printf("filesz: %d, memsz: %d\n", phdr.p_filesz, phdr.p_memsz);
       printf("end.\n");
     }
   }
