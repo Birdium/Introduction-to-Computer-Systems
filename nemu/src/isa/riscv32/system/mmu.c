ee5e78e3f7f41692bc90d94b2dceccda003bbe9a
tracer-ics2021 <tracer@njuics.org>
2021-12-20 23:13:02 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: e9fb916f264247688b5121f7a82e89e4   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  23:13:02 up  1:07,  1 user,  load average: 1.43, 0.91, 0.72

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -17,8 +17,8 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   printf("pg_dir_base: %x\n", pg_dir_base);
 
   word_t pg_dir_entry = paddr_read(pg_dir_base + 4 * dir_ndx, 4);
-  // printf("pg_dir_base: 0x%08x\n", pg_dir_base);
-  // printf("pg_dir_entry: 0x%08x\n", pg_dir_entry);
+  printf("pg_dir_base: 0x%08x\n", pg_dir_base);
+  printf("pg_dir_entry: 0x%08x\n", pg_dir_entry);
   // assert(0);
   if ((pg_dir_entry & 0x1) == 0) { // V == 0, Invalid.
     assert(0);
