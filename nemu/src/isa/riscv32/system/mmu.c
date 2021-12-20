caa32d5d2842412f53f85caf9223ee7032cf4da5
tracer-ics2021 <tracer@njuics.org>
2021-12-20 21:46:29 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: fa59298c88354722be83703ac5f192d9   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  21:46:29 up  1:21,  1 user,  load average: 1.35, 1.27, 1.11

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -15,6 +15,7 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
 
   paddr_t pg_dir_base = *satp & BASE_ADDR_MASK; // 0x3fffff originally, but we have 4GB only, the upper 2bit ignored.
 
+  assert(0);
   word_t pg_dir_entry = paddr_read(pg_dir_base + 4 * dir_ndx, 4);
   if ((pg_dir_entry & 0x1) == 0) { // V == 0, Invalid.
     assert(0);
