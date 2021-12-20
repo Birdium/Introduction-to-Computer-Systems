01b02412e047f9e955a363836245260ce9fab394
tracer-ics2021 <tracer@njuics.org>
2021-12-20 23:57:48 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: e9fb916f264247688b5121f7a82e89e4   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  23:57:48 up  1:52,  1 user,  load average: 1.15, 0.76, 0.68

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -53,6 +53,7 @@ static uintptr_t loader(PCB *pcb, const char *filename)   {
         int read_len = min(PGSIZE, faddr - vaddr);
         map(&pcb->as, (void *)(vaddr & OFFSET_MASK), paddr, PROT);
         fs_read(fd, paddr + (vaddr & OFFSET_MASK), read_len);
+        printf("paddr: %p, readlen: %d\n", paddr, read_len);
         vaddr += read_len;
       }
       assert(vaddr == faddr);
@@ -60,6 +61,7 @@ static uintptr_t loader(PCB *pcb, const char *filename)   {
       if (vaddr & OFFSET_MASK){
         int read_len = min(PGSIZE - (vaddr & OFFSET_MASK), maddr - vaddr);
         memset(paddr + (vaddr & OFFSET_MASK), 0, read_len);
+        printf("paddr: %p, readlen: %d\n", paddr, read_len);
         vaddr += read_len;
       } // in the same page, no need to allocate
       while(vaddr < maddr) {
@@ -67,6 +69,7 @@ static uintptr_t loader(PCB *pcb, const char *filename)   {
         int read_len = min(PGSIZE, maddr - vaddr);
         map(&pcb->as, (void *)(vaddr & OFFSET_MASK), paddr, PROT);
         memset(paddr + (vaddr & OFFSET_MASK), 0, read_len);
+        printf("paddr: %p, readlen: %d\n", paddr, read_len);
         vaddr += read_len;
       }
       // printf("%x %x\n", vaddr, maddr);
