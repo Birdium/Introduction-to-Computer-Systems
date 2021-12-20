d9f962d740d5e6fa29da3d99d248aa8826f4d86c
tracer-ics2021 <tracer@njuics.org>
2021-12-21 00:19:03 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: e9fb916f264247688b5121f7a82e89e4   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  00:19:03 up  2:13,  1 user,  load average: 0.76, 0.65, 0.67

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -82,10 +82,10 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
   // printf("%d %d\n", dir_ndx, table_ndx);
   PTE pg_dir_entry = pg_dir_base[dir_ndx];
   if ((pg_dir_entry & 0x1) == 0) { // V == 0, Invalid.
-    // printf("Allocating New Page.\n");
+    printf("Allocating New Page.\n");
     pg_dir_entry = (PTE)pgalloc_usr(PGSIZE); // pg_dir_entry.base_addr = "new table's base addr"
     pg_dir_base[dir_ndx] = pg_dir_entry | VALID_MASK; 
-    // printf("Allocated 0x%08x in 0x%08x\n", va, pg_dir_entry);
+    printf("Allocated 0x%08x in 0x%08x\n", va, pg_dir_entry);
   }
   PTE *pg_table_base = (PTE*) (pg_dir_entry & BASE_ADDR_MASK);
 
