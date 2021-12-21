29473c855250c7505cf37c0a98e570d8570d4c59
tracer-ics2021 <tracer@njuics.org>
2021-12-21 16:04:05 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: aa2da1e6ff4e4b6c94477545856f4ce3   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  16:04:05 up 18 min,  1 user,  load average: 0.86, 0.65, 0.49

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -149,6 +149,5 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   printf("entry: %x\n", entry);
   pcb->cp = ucontext(&pcb->as, RANGE(pcb->stack, pcb->stack + sizeof(pcb->stack)), (void*)entry);
   pcb->cp->GPRx = (uintptr_t)init_addr;
-  assert(0);
   // printf("%x %x", heap.start, heap.end);
 }
