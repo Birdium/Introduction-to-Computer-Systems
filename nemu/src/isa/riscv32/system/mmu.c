1a817d57bf286050c6a825062d08ca0bc896cdf8
tracer-ics2021 <tracer@njuics.org>
2021-12-21 16:08:12 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: aa2da1e6ff4e4b6c94477545856f4ce3   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  16:08:12 up 22 min,  1 user,  load average: 0.64, 0.66, 0.54

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -9,14 +9,14 @@
 #define satp (&cpu.csr[4]._32)
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
-  Log("vaddr: 0x%08x", vaddr);
+  // Log("vaddr: 0x%08x", vaddr);
   word_t dir_ndx  = ((uintptr_t)vaddr) >> 22,
         //  offset = ((uintptr_t)vaddr) & 0xfff,
          table_ndx = (((uintptr_t)vaddr) >> 12) & 0x3ff; 
   paddr_t pg_dir_base = *satp << 12; // 0x3fffff originally, but we have 4GB only, the upper 2bit ignored.
 
+  Log("pg_dir_entry: 0x%08x", pg_dir_base);
   word_t pg_dir_entry = paddr_read(pg_dir_base + 4 * dir_ndx, 4);
-  Log("pg_dir_entry: 0x%08x", pg_dir_entry);
   // assert(0);
   if ((pg_dir_entry & 0x1) == 0) { // V == 0, Invalid.
     assert(0);
