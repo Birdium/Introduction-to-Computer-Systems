9bf708be65795486246d8acd8eba6fcb7e3f93d4
tracer-ics2021 <tracer@njuics.org>
2021-12-22 10:42:33 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 542687ff28514944adb617831be908d3   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  10:42:33 up 31 min,  1 user,  load average: 1.67, 1.32, 1.12

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -53,9 +53,10 @@ int sys_execve(const char *filename, char *const argv[], char *const envp[]){
 }
 
 int sys_exit() {
-  char *empty[] = {NULL};
-  char *argc[] = {"bin/nterm", NULL};
-  sys_execve("/bin/nterm", argc, empty);
+  halt(0);
+  // char *empty[] = {NULL};
+  // char *argc[] = {"bin/nterm", NULL};
+  // sys_execve("/bin/nterm", argc, empty);
   return -1;
 }
 
