393bd062b00b3de6918ed8a4d843cdb0c5aaba31
tracer-ics2021 <tracer@njuics.org>
2021-12-24 20:36:17 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: b96732da07d947f9b9ee3e60f748dd88   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  20:36:17 up  3:59,  1 user,  load average: 0.66, 0.70, 0.70

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -46,7 +46,7 @@ static int u_cnt;
 #define SWAP_CNT 3000
 
 Context* schedule(Context *prev) {
-  printf("%x\n", &pcb[1]);
+  printf("%i\n", current - pcb);
   current->cp = prev;
   if (current == &pcb[0]) current = &pcb[1];
   else {
