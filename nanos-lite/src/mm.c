d2aa24cc095f66ff82acbfe534cc6d3263f476c9
tracer-ics2021 <tracer@njuics.org>
2021-12-24 17:01:32 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: b96732da07d947f9b9ee3e60f748dd88   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  17:01:32 up 24 min,  1 user,  load average: 0.39, 0.57, 0.50

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -1,4 +1,5 @@
 #include <memory.h>
+#include <proc.h>
 
 static void *pf = NULL;
 
@@ -20,9 +21,22 @@ void free_page(void *p) {
   panic("not implement yet");
 }
 
+#define OFFSET_MASK 0xfff
+#define BASE_ADDR_MASK ~OFFSET_MASK
+#define PG_ST(addr) (void*)((uintptr_t)(addr) & BASE_ADDR_MASK)
+#define PG_ED(addr) (void*)((uintptr_t)(addr) & BASE_ADDR_MASK + PGSIZE)
+#define PROT 0x7
+
 /* The brk() system call handler. */
 int mm_brk(uintptr_t brk) {
-  return 0;
+  void *va = PG_ST(current->max_brk) + PGSIZE;
+  while(va <= PG_ST(brk)) {
+    void *pa = new_page(1);
+    map(&current->as, va, pa, PROT);
+    va += PGSIZE;
+  }
+  current->max_brk = brk;
+  return 0;  
 }
 
 void init_mm() {
