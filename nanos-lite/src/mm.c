ff10d4a4ca3a8c412a59675c534ebcdbaafe784a
tracer-ics2021 <tracer@njuics.org>
2021-12-24 17:16:21 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: b96732da07d947f9b9ee3e60f748dd88   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  17:16:21 up 39 min,  1 user,  load average: 0.68, 0.71, 0.62

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -29,7 +29,8 @@ void free_page(void *p) {
 
 /* The brk() system call handler. */
 int mm_brk(uintptr_t brk) {
-  printf("entering mm_brk\n");
+  assert(0);
+  // printf("entering mm_brk\n");
   void *va = PG_ST(current->max_brk) + PGSIZE;
   while(va <= PG_ST(brk)) {
     void *pa = new_page(1);
