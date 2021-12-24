042752b60c9ed1c1fe0fa8292319e41b14e5bcb1
tracer-ics2021 <tracer@njuics.org>
2021-12-24 18:47:26 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: b96732da07d947f9b9ee3e60f748dd88   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  18:47:26 up  2:10,  1 user,  load average: 0.57, 0.87, 0.75

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -19,7 +19,6 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   // Log("pg_dir_entry: 0x%08x", pg_dir_entry);
   // assert(0);
   if ((pg_dir_entry & 0x1) == 0) { // V == 0, Invalid.
-    Log("vaddr: 0x%08x", vaddr);
     assert(0);
     return MEM_RET_FAIL;
   } 
