3141c7d9c944d7e85d5dfecdc58dd0d02b241b68
tracer-ics2021 <tracer@njuics.org>
2022-01-04 00:21:23 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 4a2320321a5e485aa01b2deec2bbef90   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  00:21:23 up  1:17,  1 user,  load average: 0.61, 0.65, 0.63

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,7 +26,7 @@ void hello_fun(void *arg) {
 #define uproc_name "/bin/nterm"
 
 void init_proc() {
-  context_kload(&pcb[1], hello_fun, "1919810");
+  context_kload(&pcb[0], hello_fun, "1919810");
   // char *argv[] = {"--skip", NULL};
   // char *envp[] = {"114514", NULL};
   char *argv[] = {uproc_name, NULL};
@@ -35,7 +35,7 @@ void init_proc() {
     printf("pcb%d : %p %p\n",  i, &pcb[i], &pcb[i].stack + sizeof (pcb[i].stack));
   }
   // context_uload(&pcb[0], "/bin/hello", argv, envp);
-  context_uload(&pcb[0], uproc_name, argv, envp);
+  context_uload(&pcb[1], uproc_name, argv, envp);
   switch_boot_pcb();
   Log("Initializing processes...");
 
