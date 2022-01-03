bd47d5453d358e085481667938c04197a36a72a5
tracer-ics2021 <tracer@njuics.org>
2022-01-04 01:06:50 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 4a2320321a5e485aa01b2deec2bbef90   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  01:06:51 up  2:03,  1 user,  load average: 0.56, 0.48, 0.48

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -135,8 +135,7 @@ void cpu_exec(uint64_t n) {
     word_t intr = isa_query_intr(s);
     if (intr != INTR_EMPTY) {
       Log("%x", intr);
-      cpu.pc = isa_raise_intr(&s, intr, cpu.pc);
-      Log("%x", cpu.pc);
+      isa_raise_intr(&s, intr, cpu.pc);
       // assert(0);
     }
   }
