5efb72aeded1d5ec56253b0137f4b23fed7e0ce3
tracer-ics2021 <tracer@njuics.org>
2022-01-03 23:57:44 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 4a2320321a5e485aa01b2deec2bbef90   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  23:57:44 up 53 min,  1 user,  load average: 0.75, 0.64, 0.57

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -24,7 +24,6 @@ word_t isa_raise_intr(Decode *s, word_t NO, vaddr_t epc) {
 word_t isa_query_intr() {
   if (cpu.INTR == true && *mstatus & MIE) {
     cpu.INTR = false;
-    assert(0);
     return IRQ_TIMER;
   }
   return INTR_EMPTY;
