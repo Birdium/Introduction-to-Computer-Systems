a3bbccbd779e6560ba440041b6b035b641755e27
tracer-ics2021 <tracer@njuics.org>
2022-01-03 11:21:40 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 6ca8fdb7a4424fc1b4e7361f1a853d94   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  11:21:40 up 17 min,  1 user,  load average: 0.48, 0.59, 0.45

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,7 +26,6 @@ void hello_fun(void *arg) {
 #define uproc_name "/bin/nterm"
 
 void init_proc() {
-  // context_kload(&pcb[0], hello_fun, "114");
   context_kload(&pcb[0], hello_fun, "1919810");
   // char *argv[] = {"--skip", NULL};
   // char *envp[] = {"114514", NULL};
