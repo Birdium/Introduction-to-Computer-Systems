86d061afdce07465dcc07efaf3237f13f8b8832a
tracer-ics2021 <tracer@njuics.org>
2022-01-04 14:33:25 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: ff5c6c9c1f60402fb840cb4134bf9d8c   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  14:33:25 up  4:19,  1 user,  load average: 0.80, 0.74, 0.78

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -109,6 +109,7 @@ Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
   Context *cp = kstack.end - sizeof(Context); 
   cp->mstatus = 0x1880;
   cp->mepc = (uintptr_t)entry;
+  printf("mepc : %p\n", cp->mepc);
   cp->pdir = as->ptr;
   printf("cp : %p\n", cp);
   return cp;
