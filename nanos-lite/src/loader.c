14d08ed2ed2bb850e5602baf2e2b7d2304bf8451
tracer-ics2021 <tracer@njuics.org>
2022-01-04 14:26:44 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: ff5c6c9c1f60402fb840cb4134bf9d8c   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  14:26:44 up  4:12,  1 user,  load average: 0.78, 0.75, 0.81

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -168,8 +168,8 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   uintptr_t entry = loader(pcb, filename);
   printf("entry: %x\n", entry);
   // pcb->cp = ucontext(&pcb->as, RANGE(pcb->stack, pcb->stack + sizeof(pcb->stack)), (void*)entry);
-  pcb->cp = ucontext(&pcb->as, RANGE(pcb->as.area.end - init_size, pcb->as.area.end), (void*)entry);
   printf("cp range: %x %x\n", pcb->as.area.end - init_size, pcb->as.area.end);
+  pcb->cp = ucontext(&pcb->as, RANGE(pcb->as.area.end - init_size, pcb->as.area.end), (void*)entry);
   pcb->cp->GPRx = (uintptr_t)pcb->as.area.end - init_size;
   for(int i = 0; i <= 31; i++) {
     printf("reg[%d] : %x\n", i, ((uintptr_t*)pcb->cp)[i]);
