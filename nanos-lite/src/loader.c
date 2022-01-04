6a63b1fd178b83baf1961ba29830a8e41a78e723
tracer-ics2021 <tracer@njuics.org>
2022-01-04 10:22:05 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: ff5c6c9c1f60402fb840cb4134bf9d8c   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  10:22:05 up 7 min,  1 user,  load average: 0.73, 0.80, 0.51

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -124,8 +124,9 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   Area ustack = {ustack_start, ustack_start + USTACK_PAGE * PGSIZE};
   // pre-process
   int argc = 0, envc = 0, str_len = 0, str_size, init_size = 0;
-  Log("Loading file: %s\n", filename);
-  Log("Address Space: %x %x\n", (uintptr_t)pcb->as.area.start, (uintptr_t)pcb->as.area.end);
+  Log("Loading file: %s", filename);
+  Log("Address Space: %x %x", (uintptr_t)pcb->as.area.start, (uintptr_t)pcb->as.area.end);
+  Log("AS->PTR: %x", (uintptr_t)pcb->as.ptr);
   // printf("%p\n", envp[0]);
   if (argv)
     while(argv[argc]) {
