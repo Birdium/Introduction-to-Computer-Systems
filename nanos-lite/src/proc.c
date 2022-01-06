04e9f49d6ea80af545095fdf33508685aecbbaae
tracer-ics2021 <tracer@njuics.org>
2022-01-06 18:38:37 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: c6001fef1aa24641bbf325bba8506f38   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  18:38:37 up 16 min,  1 user,  load average: 0.82, 0.90, 0.73

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -23,7 +23,7 @@ void hello_fun(void *arg) {
   }
 }
 
-#define uproc_name "/bin/event-test"
+#define uproc_name "/bin/file-test"
 
 void init_proc() {
   context_kload(&pcb[0], hello_fun, "1919810");
