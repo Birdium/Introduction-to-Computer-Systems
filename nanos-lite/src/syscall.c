b80eac4bd822f74d6b26d848f6b149d9979a33ec
tracer-ics2021 <tracer@njuics.org>
2022-01-06 23:46:42 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: 5ff5141332b94482bd1e102525cc8fa5   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  23:46:42 up 45 min,  1 user,  load average: 0.73, 0.42, 0.39

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -56,8 +56,8 @@ int sys_execve(const char *filename, char *const argv[], char *const envp[]){
 int sys_exit() {
   // halt(0);
   char *empty[] = {NULL};
-  char *argc[] = {"bin/dummy", NULL};
-  sys_execve("/bin/dummy", argc, empty);
+  char *argc[] = {"bin/nterm", NULL};
+  sys_execve("/bin/nterm", argc, empty);
   return -1;
 }
 
@@ -74,7 +74,7 @@ void do_syscall(Context *c) {
     case SYS_yield: yield(); c->GPRx = 0; break;
     // case SYS_yield: c->GPRx = schedule(); break;
     // case SYS_exit: halt(a[1]); break;
-    case SYS_exit: c->GPRx = sys_exit();
+    case SYS_exit: c->GPRx = sys_exit(); break;
     case SYS_open: c->GPRx = fs_open((const char*)a[1], a[2], a[3]); break; 
     case SYS_read: c->GPRx = fs_read(a[1], (void*)a[2], a[3]); break;
     case SYS_write: c->GPRx = fs_write(a[1], (void*)a[2], a[3]); break;
