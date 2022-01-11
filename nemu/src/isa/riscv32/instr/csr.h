333dbf345535c38671ca52145bf59432d5542f1b
tracer-ics2021 <tracer@njuics.org>
2022-01-11 16:26:39 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: dc6af4e48556473e9436449eed431b75   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  16:26:39 up  2:43,  1 user,  load average: 0.81, 0.65, 0.49

--- nemu/src/isa/riscv32/instr/csr.h
+++ nemu/src/isa/riscv32/instr/csr.h
@@ -21,13 +21,13 @@ def_EHelper(csrrsi) {
 }
 
 def_EHelper(csrrc) {
-    Log("mscratch before: %x", *mscratch);
-    Log("Opr before: %x, %x, %x", *ddest, id_src1->imm, *dsrc2);
+    // Log("mscratch before: %x", *mscratch);
+    // Log("Opr before: %x, %x, %x", *ddest, id_src1->imm, *dsrc2);
     rtl_neg(s, s0, dsrc1);
     rtl_mv(s, ddest, dsrc2);
     rtl_and(s, dsrc2, dsrc2, s0);
-    Log("mscratch after: %x", *mscratch);
-    Log("Opr after: %x, %x, %x", *ddest, id_src1->imm, *dsrc2);
+    // Log("mscratch after: %x", *mscratch);
+    // Log("Opr after: %x, %x, %x", *ddest, id_src1->imm, *dsrc2);
 }
 
 def_EHelper(csrrci) {
