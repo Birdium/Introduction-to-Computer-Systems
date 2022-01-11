ca4bb38cdda79335a2f60b0475f0444e422965b0
tracer-ics2021 <tracer@njuics.org>
2022-01-11 16:53:54 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: dc6af4e48556473e9436449eed431b75   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  16:53:54 up  3:10,  1 user,  load average: 0.89, 0.81, 0.76

--- nemu/src/isa/riscv32/instr/csr.h
+++ nemu/src/isa/riscv32/instr/csr.h
@@ -1,28 +1,28 @@
 def_EHelper(csrrw) {
-    Log("mscratch before: %x", *mscratch);
+    // Log("mscratch before: %x", *mscratch);
     // Log("Opr before: %x, %x, %x", *ddest, *dsrc1, *dsrc2);
     rtl_mv(s, s0, dsrc1);
     rtl_mv(s, ddest, dsrc2);
     rtl_mv(s, dsrc2, s0);
-    Log("mscratch after: %x", *mscratch);
+    // Log("mscratch after: %x", *mscratch);
     // Log("Opr after: %x, %x, %x", *ddest, *dsrc1, *dsrc2);
 }
 
 def_EHelper(csrrwi) {
-    Log("mscratch before: %x", *mscratch);
+    // Log("mscratch before: %x", *mscratch);
     // Log("Opr before: %x, %x, %x", *ddest, id_src1->imm, *dsrc2);
     rtl_mv(s, ddest, dsrc2);
     rtl_li(s, dsrc2, id_src1->imm);
-    Log("mscratch after: %x", *mscratch);
+    // Log("mscratch after: %x", *mscratch);
     // Log("Opr after: %x, %x, %x", *ddest, id_src1->imm, *dsrc2);
 }
 
 def_EHelper(csrrs) {
-    Log("mscratch before: %x", *mscratch);
+    // Log("mscratch before: %x", *mscratch);
     rtl_mv(s, s0, dsrc1);
     rtl_mv(s, ddest, dsrc2);
     rtl_or(s, dsrc2, dsrc2, s0);
-    Log("mscratch after: %x", *mscratch);
+    // Log("mscratch after: %x", *mscratch);
 }
 
 def_EHelper(csrrsi) {
@@ -36,8 +36,8 @@ def_EHelper(csrrc) {
     rtl_neg(s, s0, dsrc1);
     rtl_mv(s, ddest, dsrc2);
     // rtl_and(s, dsrc2, dsrc2, s0);
-    Log("mscratch after: %x", *mscratch);
-    Log("Opr after: %x, %x, %x", *ddest, id_src1->imm, *dsrc2);
+    // Log("mscratch after: %x", *mscratch);
+    // Log("Opr after: %x, %x, %x", *ddest, id_src1->imm, *dsrc2);
 }
 
 def_EHelper(csrrci) {
