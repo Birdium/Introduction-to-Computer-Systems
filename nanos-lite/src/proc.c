0708c6570b8207d5a34eba37c7eb01a5145916a7
tracer-ics2021 <tracer@njuics.org>
2022-01-11 21:12:28 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: dc6af4e48556473e9436449eed431b75   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  21:12:28 up  7:29,  1 user,  load average: 0.65, 0.76, 0.78

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,7 +24,7 @@ void hello_fun(void *arg) {
 }
 
 #define uproc_name_1 "/bin/nterm"
-#define uproc_name_2 "/bin/bird"
+#define uproc_name_2 "/bin/pal"
 #define uproc_name_3 "/bin/pal"
 
 void init_proc() {
@@ -53,7 +53,7 @@ void init_proc() {
 
 static int u_cnt;
 int fg_pcb = 1;
-#define SWAP_CNT 100
+#define SWAP_CNT 1000
 
 Context* schedule(Context *prev) {
   current->cp = prev;
