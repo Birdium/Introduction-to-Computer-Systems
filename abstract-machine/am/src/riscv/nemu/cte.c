078cb829fa94a7f3a107a040ad1dc7b7b51d5843
tracer-ics2021 <tracer@njuics.org>
2022-01-11 17:30:13 +0800

    >  compile 201840058    Static hostname: birdium-GL65-Leopard-10SFSK          Icon name: computer-laptop            Chassis: laptop         Machine ID: 1f456f5f68e14ee5ba55c4a8d2c0b89e            Boot ID: dc6af4e48556473e9436449eed431b75   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  17:30:13 up  3:47,  1 user,  load average: 1.11, 0.81, 0.71

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -11,7 +11,7 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 Context* __am_irq_handle(Context *c) {
   __am_get_cur_as(c);
   if (user_handler) {
-    // printf("1\n");
+    printf("sp: %x\n", c->gpr[2]);
     Event ev = {0};
     switch (c->mcause) {
       case 0xb :
